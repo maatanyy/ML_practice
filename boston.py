c08c00a21789ba250ee6e2836ce2bcac6de8cea9
maatanyy <maatanyy@naver.com>
2022-06-23 11:39:43 +0900

    생활코딩 tensorflow boston

---
+++ boston.py
@@ -0,0 +1,40 @@
+# 생활코딩 Tensorflow(python)을 참고하였습니다.
+import pandas as pd
+import tensorflow as tf
+
+
+#과거의 데이터 준비하는 부분
+filepath = 'https://raw.githubusercontent.com/blackdew/tensorflow1/master/csv/boston.csv'
+boston = pd.read_csv(filepath)
+
+
+#독립변수, 종속변수 구분
+독립 = boston[['crim', 'zn', 'indus', 'chas', 'nox', 'rm', 'age', 'dis', 'rad', 'tax','ptratio', 'b', 'lstat']]
+종속 = boston[['medv']]
+print(독립.shape, 종속.shape)
+
+
+
+# 모델의 구조를 만드는 부분
+X = tf.keras.layers.Input(shape=[13])
+Y = tf.keras.layers.Dense(1)(X)
+model = tf.keras.models.Model(X, Y)
+model.compile(loss='mse')
+
+#모델 학습 시키는 부분
+model.fit(독립, 종속, epochs= 1000)
+
+print(model.predict(독립[0:5]))
+
+
+
+
+
+
+
+
+
+
+
+
+
